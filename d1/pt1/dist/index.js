149b13356d6c555e6d4ebadd3f0e5fce1637ad91
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-01 00:05:16 -0500

    Finish  - pt1

--- d1/pt1/dist/index.js
+++ d1/pt1/dist/index.js
@@ -22,4 +22,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const rawData = fs.readFileSync('input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+let total = 0;
+for (let i = 1; i < data.length; i++) {
+    const prevNum = +data[i - 1];
+    const curNum = +data[i];
+    total += +(curNum > prevNum);
+}
+console.log(total);
+//# sourceMappingURL=index.js.map
