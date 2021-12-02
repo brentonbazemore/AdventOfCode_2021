6b98c29fb3f56143b3a8a98dca93ea4a360868c0
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-02 00:05:43 -0500

    Init d2 - pt2

---
+++ d2/pt2/dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
+const data = rawData.split('\n');
+let position = {
+    horizontal: 0,
+    depth: 0,
+};
+const move = {
+    forward: (magnitude) => { position.horizontal += magnitude; },
+    down: (magnitude) => { position.depth += magnitude; },
+    up: (magnitude) => { position.depth -= magnitude; },
+};
+data.forEach(rawMove => {
+    const [action, magnitude] = rawMove.split(' ');
+    move[action](+magnitude);
+});
+console.log(position.horizontal * position.depth);
+//# sourceMappingURL=index.js.map
