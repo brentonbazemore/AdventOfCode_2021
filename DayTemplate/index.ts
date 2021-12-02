2f4e37d3c32e574e77ea1a44dc60792e0c4b94fa
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-02 00:14:35 -0500

    Update template to include test file

--- DayTemplate/index.ts
+++ DayTemplate/index.ts
@@ -1,4 +1,8 @@
 import * as fs from 'fs';
 
-const rawData: string = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = true;
+// #################################
+
+const rawData: string = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data: string[] = rawData.split('\n');
