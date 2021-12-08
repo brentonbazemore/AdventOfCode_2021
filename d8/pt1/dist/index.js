c78d32b131c75a043a492e9a8789a486dca5b633
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-08 00:16:32 -0500

    Finish d8 - pt1

--- d8/pt1/dist/index.js
+++ d8/pt1/dist/index.js
@@ -20,6 +20,30 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
-const rawData = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+const signals = data.map((line) => {
+    const [patternString, digitString] = line.split(' | ');
+    const patterns = patternString.split(' ');
+    const digits = digitString.split(' ');
+    return {
+        patterns,
+        digits,
+    };
+});
+let total = 0;
+signals.forEach((signal) => {
+    // console.log(signal.digits);
+    signal.digits.forEach((digit) => {
+        // console.log(digit.length);
+        // 1 4 7 8
+        if (digit.length === 2 || digit.length === 4 || digit.length === 3 || digit.length === 7) {
+            total++;
+        }
+    });
+});
+console.log(total);
+//# sourceMappingURL=index.js.map
