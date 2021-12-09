f66719cd8200b6c9addbdef2897e0e70429af9d1
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-09 00:16:56 -0500

    Finish  - pt1

--- d9/pt1/dist/index.js
+++ d9/pt1/dist/index.js
@@ -20,6 +20,40 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
-const rawData = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+const WIDTH = data[0].length;
+const HEIGHT = data.length;
+const safeAdd = (grid, x, y, value) => {
+    if (!grid[x]) {
+        grid[x] = {};
+    }
+    grid[x][y] = value;
+};
+const heightMap = {};
+for (let i = 0; i < data.length; i++) {
+    const row = data[i];
+    for (let j = 0; j < row.length; j++) {
+        const cell = +row[j];
+        safeAdd(heightMap, j, i, cell);
+    }
+}
+const checkHeight = (grid, x, y) => {
+    var _a, _b, _c, _d;
+    const surrounding = [(_a = grid[x]) === null || _a === void 0 ? void 0 : _a[y + 1], (_b = grid[x + 1]) === null || _b === void 0 ? void 0 : _b[y], (_c = grid[x]) === null || _c === void 0 ? void 0 : _c[y - 1], (_d = grid[x - 1]) === null || _d === void 0 ? void 0 : _d[y]].filter((v) => v !== undefined);
+    const value = grid[x][y];
+    return value < Math.min(...surrounding);
+};
+let sum = 0;
+for (let x = 0; x < WIDTH; x++) {
+    for (let y = 0; y < HEIGHT; y++) {
+        if (checkHeight(heightMap, x, y)) {
+            sum += (heightMap[x][y] + 1);
+        }
+    }
+}
+console.log(sum);
+//# sourceMappingURL=index.js.map
--- d9/pt1/index.ts
+++ d9/pt1/index.ts
@@ -1,8 +1,49 @@
 import * as fs from 'fs';
 
 // Toggle this to switch input files
-const testInput = true;
+const testInput = false;
 // #################################
 
 const rawData: string = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data: string[] = rawData.split('\n');
+
+type Grid = { [x: number]: { [ y: number]: number }};
+
+const WIDTH = data[0].length;
+const HEIGHT = data.length;
+
+const safeAdd = (grid: Grid, x: number, y: number, value: number) => {
+  if (!grid[x]) {
+    grid[x] = {};
+  }
+
+  grid[x][y] = value;
+};
+
+const heightMap: Grid = {};
+for (let i = 0; i < data.length; i++) {
+  const row = data[i];
+  for (let j = 0; j < row.length; j++) {
+    const cell = +row[j];
+
+    safeAdd(heightMap, j, i, cell);
+  }
+}
+
+const checkHeight = (grid: Grid, x: number, y: number) => {
+  const surrounding = [ grid[x]?.[y + 1], grid[x + 1]?.[y], grid[x]?.[y - 1], grid[x - 1]?.[y] ].filter((v) => v !== undefined);
+  const value = grid[x][y];
+
+  return value < Math.min(...surrounding);
+}
+
+let sum = 0;
+for (let x = 0; x < WIDTH; x++) {
+  for (let y = 0; y < HEIGHT; y++) {
+    if (checkHeight(heightMap, x, y)) {
+      sum += (heightMap[x][y] + 1);
+    }
+  }
+}
+
+console.log(sum);
