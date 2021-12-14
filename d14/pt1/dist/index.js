3f8ff1b4b90e719fad2d2ccdb0e699a6092b9f7b
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-14 00:20:14 -0500

    Finish  - pt1

--- d14/pt1/dist/index.js
+++ d14/pt1/dist/index.js
@@ -20,6 +20,50 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
-const rawData = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+const initTemplate = data[0];
+const rules = {};
+for (let i = 2; i < data.length; i++) {
+    const [pair, insert] = data[i].split(' -> ');
+    rules[pair] = insert;
+}
+let template = initTemplate;
+for (let step = 0; step < 40; step++) {
+    console.log(step);
+    let newTemplate = '';
+    for (let i = 0; i < template.length - 1; i++) {
+        let templatePair = template[i] + template[i + 1];
+        let newSequence;
+        if (rules[templatePair]) {
+            newSequence = template[i] + rules[templatePair];
+        }
+        else {
+            console.log('hit?');
+            newSequence = templatePair;
+        }
+        newTemplate += newSequence;
+    }
+    newTemplate += template[template.length - 1];
+    template = newTemplate;
+}
+let freq = {};
+template.split('').forEach((char) => {
+    freq[char] = (freq[char] || 0) + 1;
+});
+let min = { char: '', value: Infinity };
+let max = { char: '', value: -Infinity };
+Object.keys(freq).forEach((char) => {
+    if (freq[char] > max.value) {
+        max = { char, value: freq[char] };
+    }
+    if (freq[char] < min.value) {
+        min = { char, value: freq[char] };
+    }
+});
+console.log(max.value - min.value);
+// console.log(template);
+//# sourceMappingURL=index.js.map
