eff6b7cf0a11204f4d661882793e056707d36801
Brenton Bazemore <brenton.bazemore@gmail.com>
2021-12-21 15:08:04 -0500

    Finish d21 - pt1

--- d21/pt1/dist/index.js
+++ d21/pt1/dist/index.js
@@ -20,6 +20,53 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
-const rawData = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+let [, p1Position] = data[0].split(': ').map(n => +n);
+let [, p2Position] = data[1].split(': ').map(n => +n);
+let p1Score = 0;
+let p2Score = 0;
+// 10 + 18 = 28, 18
+const wrap = (num) => {
+    while (num > 10) {
+        num -= 10;
+    }
+    return num;
+};
+let rollCount = 0;
+let die = 100;
+const getDieValue = () => {
+    die++;
+    if (die > 100) {
+        die -= 100;
+    }
+    rollCount++;
+    return die;
+};
+let isP1Turn = false;
+while (p1Score < 1000 && p2Score < 1000) {
+    isP1Turn = !isP1Turn;
+    let dist = 0;
+    for (let i = 0; i < 3; i++) {
+        dist += getDieValue();
+    }
+    if (isP1Turn) {
+        p1Position = wrap(p1Position + dist);
+        p1Score += p1Position;
+    }
+    else {
+        p2Position = wrap(p2Position + dist);
+        p2Score += p2Position;
+    }
+}
+console.log({ p1Score, p2Score, p1Position, p2Position, rollCount });
+if (p1Score > p2Score) {
+    console.log(rollCount * p2Score);
+}
+else {
+    console.log(rollCount * p1Score);
+}
+//# sourceMappingURL=index.js.map
--- d21/pt1/index.ts
+++ d21/pt1/index.ts
@@ -1,8 +1,60 @@
 import * as fs from 'fs';
 
 // Toggle this to switch input files
-const testInput = true;
+const testInput = false;
 // #################################
 
 const rawData: string = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data: string[] = rawData.split('\n');
+
+let [, p1Position] = data[0].split(': ').map(n => +n);
+let [, p2Position] = data[1].split(': ').map(n => +n);
+
+let p1Score = 0;
+let p2Score = 0;
+
+const wrap = (num: number) => {
+  while (num > 10) {
+    num -= 10;
+  }
+
+  return num;
+}
+
+let rollCount = 0;
+let die = 100;
+const getDieValue = () => {
+  die++;
+  if (die > 100) {
+    die -= 100;
+  }
+  rollCount++;
+
+  return die;
+};
+
+let isP1Turn = false;
+while (p1Score < 1000 && p2Score < 1000) {
+  isP1Turn = !isP1Turn;
+
+  let dist = 0;
+  for (let i = 0; i < 3; i++) {
+    dist += getDieValue();
+  }
+
+  if (isP1Turn) {
+    p1Position = wrap(p1Position + dist);
+    p1Score += p1Position
+  } else {
+    p2Position = wrap(p2Position + dist);
+    p2Score += p2Position;
+  }
+}
+
+console.log({ p1Score, p2Score, p1Position, p2Position, rollCount });
+
+if (p1Score > p2Score) {
+  console.log(rollCount * p2Score);
+} else {
+  console.log(rollCount * p1Score);
+}
