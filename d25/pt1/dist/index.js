d4df84f6f0d42472e04f53853c6821c989c37a6c
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-01-11 12:56:37 -0500

    Finish d25 - pt2

--- d25/pt1/dist/index.js
+++ d25/pt1/dist/index.js
@@ -20,6 +20,81 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
-const rawData = fs.readFileSync('input.txt', 'utf8');
+// Toggle this to switch input files
+const testInput = false;
+// #################################
+const rawData = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data = rawData.split('\n');
-console.log(data);
+const HEIGHT = data.length;
+const WIDTH = data[0].length;
+let cucumberGrid = {};
+for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+        if (!cucumberGrid[x]) {
+            cucumberGrid[x] = {};
+        }
+        cucumberGrid[x][y] = data[y][x];
+    }
+}
+const moveEast = () => {
+    let newGrid = JSON.parse(JSON.stringify(cucumberGrid));
+    let diff = 0;
+    for (let x = 0; x < WIDTH; x++) {
+        for (let y = 0; y < HEIGHT; y++) {
+            const nextX = (x + 1) % WIDTH;
+            if (cucumberGrid[x][y] === '>' && cucumberGrid[nextX][y] === '.') {
+                // then move it
+                newGrid[x][y] = '.';
+                newGrid[nextX][y] = '>';
+                diff++;
+            }
+            else {
+                // leave it
+            }
+        }
+    }
+    cucumberGrid = newGrid;
+    return diff;
+};
+const moveDown = () => {
+    let newGrid = JSON.parse(JSON.stringify(cucumberGrid));
+    let diff = 0;
+    for (let x = 0; x < WIDTH; x++) {
+        for (let y = 0; y < HEIGHT; y++) {
+            const nextY = (y + 1) % HEIGHT;
+            if (cucumberGrid[x][y] === 'v' && cucumberGrid[x][nextY] === '.') {
+                // then move it
+                diff++;
+                newGrid[x][y] = '.';
+                newGrid[x][nextY] = 'v';
+            }
+            else {
+                // leave it
+            }
+        }
+    }
+    cucumberGrid = newGrid;
+    return diff;
+};
+const logGrid = () => {
+    for (let y = 0; y < HEIGHT; y++) {
+        let line = '';
+        for (let x = 0; x < WIDTH; x++) {
+            line += cucumberGrid[x][y];
+        }
+        console.log(line);
+    }
+};
+let i = 0;
+while (true) {
+    let diff = 0;
+    diff += moveEast();
+    diff += moveDown();
+    if (diff === 0) {
+        break;
+    }
+    i++;
+}
+logGrid();
+console.log(i + 1, 'steps');
+//# sourceMappingURL=index.js.map
--- d25/pt1/index.ts
+++ d25/pt1/index.ts
@@ -1,8 +1,96 @@
 import * as fs from 'fs';
 
 // Toggle this to switch input files
-const testInput = true;
+const testInput = false;
 // #################################
 
 const rawData: string = fs.readFileSync(testInput ? 'inputTest.txt' : 'input.txt', 'utf8');
 const data: string[] = rawData.split('\n');
+
+const HEIGHT = data.length;
+const WIDTH = data[0].length;
+
+type Grid = { [x: number]: { [y: number]: '.' | '>' | 'v' }};
+
+let cucumberGrid: Grid = {};
+for (let y = 0; y < HEIGHT; y++) {
+  for (let x = 0; x < WIDTH; x++) {
+    if (!cucumberGrid[x]) {
+      cucumberGrid[x] = {};
+    }
+
+    cucumberGrid[x][y] = data[y][x] as '.' | '>' | 'v';
+  }
+}
+
+const moveEast = () => {
+  let newGrid: Grid = JSON.parse(JSON.stringify(cucumberGrid));
+
+  let diff = 0;
+  for (let x = 0; x < WIDTH; x++) {
+    for (let y = 0; y < HEIGHT; y++) {
+      const nextX = (x + 1) % WIDTH;
+      if (cucumberGrid[x][y] === '>' && cucumberGrid[nextX][y] === '.') {
+        // then move it
+        newGrid[x][y] = '.';
+        newGrid[nextX][y] = '>';
+        diff++;
+      } else {
+        // leave it
+      }
+    }
+  }
+
+  cucumberGrid = newGrid;
+
+  return diff;
+}
+
+const moveDown = () => {
+  let newGrid: Grid = JSON.parse(JSON.stringify(cucumberGrid));
+
+  let diff = 0;
+  for (let x = 0; x < WIDTH; x++) {
+    for (let y = 0; y < HEIGHT; y++) {
+      const nextY = (y + 1) % HEIGHT;
+      if (cucumberGrid[x][y] === 'v' && cucumberGrid[x][nextY] === '.') {
+        // then move it
+        diff++;
+        newGrid[x][y] = '.';
+        newGrid[x][nextY] = 'v';
+      } else {
+        // leave it
+      }
+    }
+  }
+
+  cucumberGrid = newGrid;
+
+  return diff;
+}
+
+const logGrid = () => {
+  for (let y = 0; y < HEIGHT; y++) {
+    let line = '';
+    for (let x = 0; x < WIDTH; x++) {
+      line += cucumberGrid[x][y];
+    }
+    console.log(line);
+  }
+}
+
+let i = 0;
+while (true) {
+  let diff = 0;
+  diff += moveEast();
+  diff += moveDown();
+
+  if (diff === 0) {
+    break;
+  }
+
+  i++;
+}
+
+logGrid();
+console.log(i + 1, 'steps');
